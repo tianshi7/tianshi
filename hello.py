e0c097945e5dfe91074a71618f069921acfe5fe8
tianhe <ittianshi@gmail.com>
2016-01-18 20:44:07 +0800

    a

--- hello.py
+++ hello.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask app = Flask(__name__)
+from flask import Flask,request,render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
-@app.route('/') def index():
-    return '<h1>Hello World!</h1>h1>'
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+manager = Manager((app))
+@app.route('/')
+def index():
+    #return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	#return '<h1>hello %s' % name
+    return render_template('user.html',name=name)
+@app.route('/agent')
+def agent():
+	user_agent = request.headers.get('User-Agent')
+	return '<p>you browser is %s</p>' % user_agent
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
