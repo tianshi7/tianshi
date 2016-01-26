84e791897c47f2f7bfd4820037ca205ab9f6fcf1
tianhe <ittianshi@gmail.com>
2016-01-26 19:02:31 +0800

    a

--- hello.py
+++ hello.py
@@ -1,26 +1,86 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template,session,redirect,url_for,flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Shell
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+class NameForm(Form):
+    name = StringField('what is your name?',validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI']=\
+        'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']= True
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 manager = Manager((app))
-@app.route('/')
+manager.add_command("shell",Shell(make_context=make_shell_context))
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r' % self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    #return '<h1>Hello World!</h1>'
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('look like you have changed your name!')
+        session['name']= form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           current_time=datetime.utcnow(),
+                    form=form,name=session.get('name'))
+
 
 @app.route('/user/<name>')
 def user(name):
-	#return '<h1>hello %s' % name
-    return render_template('user.html',name=name)
-@app.route('/agent')
-def agent():
-	user_agent = request.headers.get('User-Agent')
-	return '<p>you browser is %s</p>' % user_agent
+    # return '<h1>hello %s' % name
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     manager.run()
